776e693f683c33cc92feed00f57fdda33d89c66d
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-19 15:02:27 +0300

    start

---
+++ RocketApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RocketApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
